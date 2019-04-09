78afd22238726ecb1499588cf757209e38dec8e6
obviousrebel <jalexander@stsci.edu>
2019-04-09 09:43:33 -0400

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# RSA-Py
+
+Simple RSA Decrypter for n up to 100,000.
+
+## Support
+
+None.
+
+## Requirements
+
+
+## Usage
+
+```
+
+```
+
+## Examples
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+PACKAGENAME='rsa'
+
+setup(name='rsa',
+      version='0.1.0',
+      description='Simple RSA Decrypter for n up to 100,000',
+      author='Joshua Alexander',
+      author_email='itzjoshy8@gmail.com',
+      url='',
+      install_requires = [
+      ],
+      packages=find_packages(),
+      )
